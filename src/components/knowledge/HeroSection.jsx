b0f1ed17022c3739fedd9c55584929f52a6eae6e
J C <jonathantcross@gmail.com>
2025-11-19 20:48:15 +0000

    File changes

--- src/components/knowledge/HeroSection.jsx
+++ src/components/knowledge/HeroSection.jsx
@@ -33,20 +33,20 @@ export default function HeroSection() {
             <Button 
               size="lg"
               variant="outline"
-              className="border-white text-white hover:bg-white/10"
+              className="border-white text-white hover:bg-white/10 hover:text-white"
               onClick={() => navigate(createPageUrl("LearningPacks"))}
             >
               <BookOpen className="w-5 h-5 mr-2" />
-              Browse Learning Packs
+              <span>Browse Learning Packs</span>
             </Button>
             <Button 
               size="lg"
               variant="outline"
-              className="border-white text-white hover:bg-white/10"
+              className="border-white text-white hover:bg-white/10 hover:text-white"
               onClick={() => navigate(createPageUrl("MyLibrary"))}
             >
               <Library className="w-5 h-5 mr-2" />
-              Go to My Library
+              <span>Go to My Library</span>
             </Button>
           </div>
         </div>
