bc7385b89f2605349b6bc2dfc1c0e40e797f7bba
J C <jonathantcross@gmail.com>
2025-12-22 18:39:58 +0000

    File changes

--- functions/analyzeNewsletterUrl.ts
+++ functions/analyzeNewsletterUrl.ts
@@ -105,8 +105,8 @@ Extract:
     console.log('AI analysis complete:', result.title);
     console.log('Creating newsletter record...');
 
-    // Create newsletter
-    await base44.asServiceRole.entities.Newsletter.create(newsletterData);
+    // Create newsletter (use regular entities since user is already authenticated as admin)
+    await base44.entities.Newsletter.create(newsletterData);
 
     console.log('Newsletter created successfully');
 
